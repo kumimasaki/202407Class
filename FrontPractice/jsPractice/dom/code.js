bbfc39f5a964a55d278dc1719feb4e71bf0c0939
kumimasaki <masaki.kmkm@gmail.com>
2024-07-31 14:53:21 +0900

    CSS dom

---
+++ FrontPractice/jsPractice/dom/code.js
@@ -0,0 +1,17 @@
+// 要素の取得
+let text = document.getElementById("text-1");
+// 中身の確認
+console.log(text);
+// 要素の内容を取得
+console.log(text.innerHTML);
+// 要素の内容を変更
+text.innerHTML = "Hello";
+
+// 要素の取得
+let text2 = document.getElementById("text-2");
+// 中身の確認
+console.log(text2);
+// 要素の内容を取得
+console.log(text2.innerHTML);
+// 要素の内容を変更(Bye)
+text2.innerHTML = "Bye";
