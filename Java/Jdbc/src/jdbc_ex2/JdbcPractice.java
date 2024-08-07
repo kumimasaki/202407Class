3bf51fa174e5fc3505529c40b4adb02a798e06fb
kumimasaki <masaki.kmkm@gmail.com>
2024-08-07 12:53:38 +0900

    jdbc practice

---
+++ Java/Jdbc/src/jdbc_ex2/JdbcPractice.java
@@ -0,0 +1,19 @@
+package jdbc_ex2;
+
+public class JdbcPractice {
+
+	public static void main(String[] args) {
+		// メソッドの呼び出し
+	}
+	
+    // Hello倉庫の中のポケモンテーブルから全てのデータを抽出して出力してください
+	// 既存の.propertiesファイルを使うこと
+	// practiceメソッドを作成
+	
+	//	⇒出力結果
+	//	id:37,name:Vulpix,type_id:10
+	//	id:46,name:Paras,type_id:7
+	//	id:133,name:Eevee,type_id:1
+
+
+}
