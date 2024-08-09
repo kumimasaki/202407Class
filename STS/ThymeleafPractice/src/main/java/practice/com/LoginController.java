15fa62f235a5dae644335002397cd03b51673021
kumimasaki <masaki.kmkm@gmail.com>
2024-08-09 15:51:43 +0900

    sts model

--- STS/ThymeleafPractice/src/main/java/practice/com/LoginController.java
+++ STS/ThymeleafPractice/src/main/java/practice/com/LoginController.java
@@ -1,7 +1,11 @@
 package practice.com;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
@@ -9,6 +13,21 @@ public class LoginController {
 	@GetMapping("/login")
 	public String getLoginPage() {
 		return "login.html";
+	}
+	
+//	@PostMapping("/login/process")
+//	public String login(@RequestParam String email, @RequestParam String password, Model model) {
+//		model.addAttribute("userEmail", email);
+//		model.addAttribute("userPassword", password);
+//		return "success.html";
+//	}
+	
+	@PostMapping("/login/process")
+	public ModelAndView login(@RequestParam String email, @RequestParam String password, ModelAndView mav) {
+		mav.addObject("userEmail", email);
+		mav.addObject("userPassword", password);
+		mav.setViewName("success.html");
+		return mav;
 	}
 	
 }
